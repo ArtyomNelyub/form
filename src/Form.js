2b6f3ec73f48399a7565a22a8f0ebfe5b1566d72
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2022-09-28 18:57:04 +0400

    Добавит функционал вывода данных формы через alert при нажатии на кнопку 'отправить'

--- src/Form.js
+++ src/Form.js
@@ -3,11 +3,14 @@ import React from 'react';
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.clickHandler = this.clickHandler.bind(this);
-  }
-
-  clickHandler() {
-    console.log(1);
+    this.nameInput = React.createRef();
+    this.surnameInput = React.createRef();
+    this.phoneInput = React.createRef();
+    this.birthdayInput = React.createRef();
+    this.websiteInput = React.createRef();
+    this.aboutInput = React.createRef();
+    this.stackInput = React.createRef();
+    this.lastProjectInput = React.createRef();
   }
 
   render() {
@@ -18,7 +21,13 @@ class Form extends React.Component {
         <div className='form__block block'>
           <div className='form__field block__half'>
             <label htmlFor='name'>Имя:</label>
-            <input type='text' name='name' id='name' placeholder='Имя' />
+            <input
+              type='text'
+              name='name'
+              id='name'
+              placeholder='Имя'
+              ref={this.nameInput}
+            />
           </div>
           <div className='form__field block__half'>
             <label htmlFor='surname'>Фамилия:</label>
@@ -27,6 +36,7 @@ class Form extends React.Component {
               name='surname'
               id='surname'
               placeholder='Фамилия'
+              ref={this.surnameInput}
             />
           </div>
         </div>
@@ -34,7 +44,13 @@ class Form extends React.Component {
         <div className='form__block block'>
           <div className='form__field block__half'>
             <label htmlFor='phone'>Телефон:</label>
-            <input type='tel' name='phone' id='phone' placeholder='Телефон' />
+            <input
+              type='tel'
+              name='phone'
+              id='phone'
+              placeholder='Телефон'
+              ref={this.phoneInput}
+            />
           </div>
           <div className='form__field block__half'>
             <label htmlFor='birthday'>Дата рождения:</label>
@@ -43,13 +59,20 @@ class Form extends React.Component {
               name='birthday'
               id='birthday'
               placeholder='Дата рождения'
+              ref={this.birthdayInput}
             />
           </div>
         </div>
 
         <div className='form__field'>
           <label htmlFor='website'>Сайт:</label>
-          <input type='url' name='website' id='website' placeholder='Сайт' />
+          <input
+            type='url'
+            name='website'
+            id='website'
+            placeholder='Сайт'
+            ref={this.websiteInput}
+          />
         </div>
 
         <div className='form__field'>
@@ -59,6 +82,7 @@ class Form extends React.Component {
             id='about'
             rows='7'
             placeholder='О себе'
+            ref={this.aboutInput}
           ></textarea>
         </div>
 
@@ -69,6 +93,7 @@ class Form extends React.Component {
             id='stack'
             rows='7'
             placeholder='Стек технологий'
+            ref={this.stackInput}
           ></textarea>
         </div>
 
@@ -79,6 +104,7 @@ class Form extends React.Component {
             id='last-project'
             rows='7'
             placeholder='Описание последнего проекта'
+            ref={this.lastProjectInput}
           ></textarea>
         </div>
 
@@ -86,7 +112,23 @@ class Form extends React.Component {
           <button type='reset' className='button button_clear'>
             Очистить
           </button>
-          <button type='button' className='button' onClick={this.clickHandler}>
+          <button
+            type='button'
+            className='button'
+            onClick={() => {
+              const data = {
+                name: this.nameInput.current.value,
+                surname: this.surnameInput.current.value,
+                phone: this.phoneInput.current.value,
+                birthday: this.birthdayInput.current.value,
+                website: this.websiteInput.current.value,
+                about: this.aboutInput.current.value,
+                stack: this.stackInput.current.value,
+                lastProject: this.lastProjectInput.current.value,
+              };
+              alert(JSON.stringify(data, null, 2));
+            }}
+          >
             Отправить
           </button>
         </div>
