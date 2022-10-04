145473764fa9b2d7e55eb1ee38be4794192549d5
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2022-10-04 09:27:03 +0400

    Доработает компонент input-field-phone

--- src/input-field-phone.js
+++ src/input-field-phone.js
@@ -1,32 +1,57 @@
-import { useState } from 'react';
 import ErrorBlock from './error-block';
 
 const regExNumb = /(\d)|(-)/;
 const NUMBER_COUNT = 12;
 
 export default function InputFieldPhone(props) {
-  const [isError, setIsError] = useState(false);
-  const [isCorrect, setIsCorrect] = useState(true);
   const { name, placeholder, type, values, setValues } = props;
+  const { value, isCorrect, isError } = values;
 
   function numberHandler(str) {
     str = str.split('-').join('');
     let mask = 'x-xxxx-xx-xx';
     let count = 0;
-    if (str.length >= 2) {
-      count = 1;
-    }
-    if (str.length >= 6) {
-      count = 2;
-    }
-    if (str.length >= 8) {
-      count = 3;
-    }
+    if (str.length >= 2) count = 1;
+    if (str.length >= 6) count = 2;
+    if (str.length >= 8) count = 3;
+
     for (let i = 0; i < str.length; i++) {
       mask = mask.replace('x', str[i]);
     }
+
     mask = mask.slice(0, str.length + count);
+
     return mask;
+  }
+
+  function onChangeHandler(e) {
+    if (
+      !regExNumb.test(e.target.value[e.target.value.length - 1]) &&
+      e.target.value !== ''
+    ) {
+      return;
+    }
+
+    setValues((prev) => ({
+      ...prev,
+      value: numberHandler(e.target.value),
+    }));
+  }
+
+  function onBlurHandler(e) {
+    setValues((prev) => ({
+      ...prev,
+      isError: !value,
+      isCorrect: !(value.length < NUMBER_COUNT && value !== ''),
+    }));
+  }
+
+  function onFocusHandler() {
+    setValues((prev) => ({
+      ...prev,
+      isCorrect: true,
+      isError: false,
+    }));
   }
 
   return (
@@ -44,33 +69,10 @@ export default function InputFieldPhone(props) {
         maxLength={NUMBER_COUNT}
         id={name}
         placeholder={placeholder}
-        value={values[name]}
-        onChange={(e) => {
-          if (
-            !regExNumb.test(e.target.value[e.target.value.length - 1]) &&
-            e.target.value !== ''
-          ) {
-            return;
-          }
-
-          setValues((prev) => ({
-            ...prev,
-            [name]: numberHandler(e.target.value),
-          }));
-        }}
-        onBlur={(e) => {
-          if (values[name]) {
-            setIsError(false);
-          } else setIsError(true);
-
-          if (values[name].length < NUMBER_COUNT && values[name] !== '') {
-            setIsCorrect(false);
-          } else setIsCorrect(true);
-        }}
-        onFocus={() => {
-          setIsCorrect(true);
-          setIsError(false);
-        }}
+        value={value}
+        onChange={onChangeHandler}
+        onBlur={onBlurHandler}
+        onFocus={onFocusHandler}
       />
     </div>
   );
