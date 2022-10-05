bb361814d6c764253313900f7f826e59b80d8f54
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2022-10-05 14:09:42 +0400

    Добавит компонент not-found

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,17 @@
 import { useState } from 'react';
-import Summary from './summary';
-import Form from './form';
+
+import Summary from './summary/summary';
+import Form from './form/form';
+import NotFound from './not-found/not-found';
 
 export default function App() {
   const [isSummaryOpen, setIsSummaryOpen] = useState(false);
   const [summaryProps, setSummaryProps] = useState([]);
   return (
-    <>
-      {!isSummaryOpen && <Form summaryOpenHandler={setIsSummaryOpen} summaryValuesHandler={setSummaryProps}/>}
-      {isSummaryOpen && <Summary values={summaryProps} summaryOpenHandler={setIsSummaryOpen}/>}
-    </>
+   <NotFound />
+    // <>
+    //   {!isSummaryOpen && <Form summaryOpenHandler={setIsSummaryOpen} summaryValuesHandler={setSummaryProps}/>}
+    //   {isSummaryOpen && <Summary values={summaryProps} summaryOpenHandler={setIsSummaryOpen}/>}
+    // </>
   );
 }
---
+++ src/components/not-found/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className='not-found'>
+      <h1 className='not-found__header'>404!</h1>
+      <p className='not-found__footer'>Страница не найдена!</p>
+    </div>
+  );
+}
